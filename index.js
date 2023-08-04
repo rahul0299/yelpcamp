b3ee4531fb9758b8c2211323e0eeabd4e3bddeaf
rahul0299 <rahulbhargava1999@gmail.com>
2023-08-04 14:41:34 +0530

    show route for single camp

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ app.set('view engine', 'ejs');
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render("campgrounds/index", { campgrounds });
-})
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", { campground });
+});
 
 app.get('/', (req, res) => {
     res.render("home");
