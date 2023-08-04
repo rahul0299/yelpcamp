9e0f00dca11eab3674f884eecc92098bc39994f5
rahul0299 <rahulbhargava1999@gmail.com>
2023-08-04 12:55:45 +0530

    created basic camp model, route to create new camp

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+    .then(() => console.log("Connected to DB"))
+    .catch(err => console.log("Error connecting to DB", err));
 
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
+
+app.get('/makeCampground', (req, res) => {
+    const camp = new Campground({ title: 'My Backyard'});
+    console.log(camp);
+    res.send(camp);
+})
 
 app.get('/', (req, res) => {
     res.render("home");
