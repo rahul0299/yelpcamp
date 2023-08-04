8e3bd27224727f44c7505cdcb96fdf83071d293e
rahul0299 <rahulbhargava1999@gmail.com>
2023-08-04 15:08:22 +0530

    update camp route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(() => console.log("Connected to DB"))
@@ -11,7 +12,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -27,11 +29,22 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render("campgrounds/show", { campground });
 });
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
+});
+
 app.post('/campgrounds', async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground.id}`);
-})
+});
+
+app.patch('/campgrounds/:id', async (req, res) => {
+    const camp = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${camp.id}`);
+
+});
 
 app.get('/', (req, res) => {
     res.render("home");
