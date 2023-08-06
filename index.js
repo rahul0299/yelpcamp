cb9a3f6563b304381406f35b4acf5a454d406e32
rahul0299 <rahulbhargava1999@gmail.com>
2023-08-06 12:16:45 +0530

    created error page template

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const catchAsync = require('./utils/catchAsync')
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(() => console.log("Connected to DB"))
@@ -38,6 +39,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
 }));
 
 app.post('/campgrounds', catchAsync(async (req, res) => {
+    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     console.log(campground);
     await campground.save();
@@ -59,10 +61,15 @@ app.get('/', (req, res) => {
     res.render("home");
 });
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+})
+
 app.use((err, req, res, next) => {
+    const { message = "Something went wrong", statusCode = 500} = err;
     console.log(err.message);
     console.log(err);
-    res.send("Something went wrong" + err)
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
