b8281ae6848d4f15eda3401b4e756402851bfe67
rahul0299 <rahulbhargava1999@gmail.com>
2023-08-13 17:48:13 +0530

    added flash messages for errors

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -26,11 +26,19 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if (!campground) {
+        req.flash('error', 'Cannot find campground');
+        return res.redirect(`/campgrounds`);
+    }
     res.render("campgrounds/show", { campground });
 }));
 
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+        req.flash('error', 'Cannot find campground');
+        return res.redirect(`/campgrounds`);
+    }
     res.render("campgrounds/edit", { campground });
 }));
 
