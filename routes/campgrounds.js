d8d2d1f32d49fc587cc18c5f88dee0662353ead8
rahul0299 <rahulbhargava1999@gmail.com>
2023-08-16 21:13:51 +0530

    cloudinary for image uploading

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,9 @@ const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware'); 
 const campgrounds = require('../controllers/campgrounds');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
@@ -16,6 +19,10 @@ router.route('/:id')
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files)
+        res.send("Files uploaded");
+    });
 
 module.exports = router;
